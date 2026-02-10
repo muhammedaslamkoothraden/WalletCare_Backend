05cc9f221b261cd178e42715fd1516ce2123212f
akbarinGit <akbarshaheed10@gmail.com>
2026-02-10 11:31:44 +0530

    validation completed for handling goals

--- src/controllers/goal.controller.js
+++ src/controllers/goal.controller.js
@@ -3,11 +3,23 @@ const Goal = require("../models/Goal");
 // CREATE GOAL
 exports.createGoal = async (req, res) => {
   try {
+    // 1️⃣ Take data from request body
+    const { title, targetAmount, targetDate } = req.body;
+
+    // 2️⃣ Create goal in database
+    
+    //  const goal = await Goal.create({
+    //     userId: req.user.id,
+    //     ...req.body,
+    //   });
+
     const goal = await Goal.create({
-      userId: req.user.id,
-      ...req.body,
+      title,
+      targetAmount,
+      targetDate,
     });
 
+    // 3️⃣ Send response
     res.status(201).json({
       success: true,
       message: "Goal created successfully",
@@ -24,9 +36,7 @@ exports.createGoal = async (req, res) => {
 // GET ALL GOALS
 exports.getGoals = async (req, res) => {
   try {
-    const goals = await Goal.find({
-      userId: req.user.id,
-    });
+    const goals = await Goal.find();
 
     res.status(200).json({
       success: true,
--- src/models/Goal.js
+++ src/models/Goal.js
@@ -2,11 +2,16 @@ const mongoose = require("mongoose");
 
 const goalSchema = new mongoose.Schema(
     {
+        // userId: {
+        //     type: mongoose.Schema.Types.ObjectId,
+        //     ref: "User",
+        //     required: true,
+        // },
         userId: {
             type: mongoose.Schema.Types.ObjectId,
-            ref: "User",
-            required: true,
-        },
+            ref: "User"
+        }
+        ,
         title: {
             type: String,
             required: true,
