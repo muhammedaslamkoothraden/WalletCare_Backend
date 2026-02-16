c639fd524e3795f87ed9726d8ed16ca023f085f8
muhammedaslamkoothraden <muhammedaslam.koothraden@gmail.com>
2026-02-16 14:47:46 +0530

    feat(wallet): add  wallet balance endpoint

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const connectDB = require("./config/db");
 const devwallet = require('./routes/dev.wallet.routes');
-
+const wallet =  require('./routes/wallet.routes');
 // Load environment variables
 require("dotenv").config();
 
@@ -13,6 +13,7 @@ const app = express();
 // Middleware
 app.use(express.json());
 app.use(  "/api/dev/wallet", devwallet);
+app.use("/api/wallet", wallet);
 
 // Example route
 app.get("/", (req, res) => {
